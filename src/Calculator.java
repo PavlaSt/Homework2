6b42a26610805eb91f04661864692a96d596bf6e
PavlaSt <54997970+PavlaSt@users.noreply.github.com>
2020-07-24 15:05:59 +0200

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,81 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Calculator {
+    //int a;
+    //int b;
+
+    public static void main(String[] args) {
+        boolean checker = true;
+        while (checker) {
+            Scanner scanner = new Scanner(System.in);
+            int a;
+            int b;
+            String operator;
+//          get variables
+            while (true) {
+                try {
+                    System.out.println("Zadej a: ");
+                    a = Integer.parseInt(scanner.next());
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("Chybný vstup");
+                }
+            }
+            while (true) {
+                try {
+                    System.out.println("Zadej b: ");
+                    b = Integer.parseInt(scanner.next());
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("Chybný vstup");
+                }
+            }
+            while (true) {
+                try {
+                    System.out.println("Zadej operator: ");
+                    operator = scanner.next();
+                    if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")) {
+                        break;
+                    } else {
+                        System.out.println("Zadej znovu operátor.");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Chybný vstup");
+                }
+            }
+
+//          perform operation and print result
+            switch (operator) {
+                case "+":
+                    System.out.println(a + b);
+                    break;
+                case "-":
+                    System.out.println(a - b);
+                    break;
+                case "*":
+                    System.out.println(a * b);
+                    break;
+                case "/":
+                    try {
+                        System.out.println(a / b);
+                        break;
+                    } catch (ArithmeticException e) {
+                        System.out.println("Nulou dělit nelze! Je nutno zadat jinou hodnotu b nebo jiný operátor.");
+                        break;
+                    }
+                default:
+                    System.out.println("Chybný vstup");
+                    break;
+            }
+//          decide if continue
+            System.out.println("Chceš pokračovat ?  ANO / NE ");
+            String goOn = scanner.next();
+
+            if (goOn.toUpperCase().equals("NE")) {
+                checker = false;
+                break;
+            }
+        }
+    }
+}
---
+++ src/Statistics.java
@@ -0,0 +1,102 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Statistics {
+    public static void main(String[] args) {
+        System.out.println("Program Statistika.");
+        System.out.println("-------------------");
+        System.out.println("Ze zadaných celých čísel spočítá minimum, maximum a průměr.");
+        boolean operationChecker = true;
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            int countOfNumbers;
+            int[] numbers;
+            String operation;
+
+//          get input data
+            while (true) {
+                try {
+                    System.out.println("Zadej počet vkládaných hodnot: ");
+                    countOfNumbers = Integer.parseInt(scanner.next());
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("Chybný vstup");
+                }
+            }
+            numbers = new int[countOfNumbers];
+            while (true) {
+                try {
+                    for (int i = 0; i < countOfNumbers; i++) {
+                        System.out.printf("Zadej %d. hodnotu:  ", i + 1);
+                        numbers[i] = Integer.parseInt(scanner.next());
+                    }
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("Chybný vstup");
+                }
+            }
+            while (operationChecker) {
+//          get operation
+                while (true) {
+                    try {
+                        System.out.println("Zadej operaci: MIN, MAX, AVG ");
+                        operation = scanner.next();
+                        if (operation.equals("MIN") || operation.equals("MAX") || operation.equals("AVG")) {
+                            break;
+                        } else {
+                            System.out.println("Zadej správnou operaci: MIN, MAX nebo AVG.");
+                        }
+                    } catch (InputMismatchException e) {
+                        System.out.println("Chybný vstup.");
+                    }
+                }
+
+//          perform operation and print result
+                switch (operation) {
+                    case "MIN":
+                        int min = Integer.MAX_VALUE;
+                        for (int i = 0; i < countOfNumbers; i++) {
+                            if (numbers[i] < min) {
+                                min = numbers[i];
+                            }
+                        }
+                        System.out.printf("Minimum je %d.", min);
+                        break;
+                    case "MAX":
+                        int max = Integer.MIN_VALUE;
+                        for (int i = 0; i < countOfNumbers; i++) {
+                            if (numbers[i] > max) {
+                                max = numbers[i];
+                            }
+                        }
+                        System.out.printf("Maximum je %d.", max);
+                        break;
+                    case "AVG":
+                        int sum = 0;
+                        for (int i = 0; i < countOfNumbers; i++) {
+                            sum += numbers[i];
+                        }
+                        System.out.printf("Průměr je %.2f.", sum / (double) countOfNumbers);
+                        break;
+                    default:
+                        System.out.println("Chybný vstup");
+                        break;
+                }
+//          decide if continue with different operation
+                System.out.println(" Chceš pokračovat další operací operací?  ANO / NE ");
+                if (scanner.next().toUpperCase().equals("NE")) {
+                    operationChecker = false;
+                    break;
+                }
+            }
+
+//          decide if continue  with different data
+            System.out.println("Chceš pokračovat s jinými daty?  ANO / NE ");
+            String goOn = scanner.next();
+
+            if (goOn.toUpperCase().equals("NE")) {
+                break;
+            }
+        }
+    }
+}
